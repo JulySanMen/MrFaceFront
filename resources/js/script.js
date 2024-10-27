5969b9ea1d5e79358338e1a65a8b5d342ccb9e0e
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-10-26 19:33:10 -0600

    front

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,26 +1,36 @@
- 
-        async function uploadImage() {
+document.getElementById("uploadForm").onsubmit = async function(event){
             const input = document.getElementById('fileInput');
             if (input.files.length === 0) {
                 alert("Por favor, selecciona una imagen.");
                 return;
             }
+            
             const formData = new FormData();
             formData.append('file', input.files[0]);
 
-            const response = await fetch('https://mrface.onrender.com/upload', {
-                method: 'POST',
-                body: formData,
-            });
+            // Mostrar el spinner de carga
+            document.getElementById('loading').style.display = 'block';
 
-            const data = await response.json();
+            try {
+                const response = await fetch('https://mrface.onrender.com/upload', {
+                    method: 'POST',
+                    body: formData,
+                });
 
-            // Mostrar la imagen procesada
-            const processedImage = document.getElementById('processedImage');
-            processedImage.src = 'data:image/png;base64,' + data.image_with_points_base64;
-            processedImage.style.display = 'block';
+                const data = await response.json();
 
-            console.log('Imagen original subida con ID:', data.drive_id);
-        }
-   
+                // Ocultar el spinner de carga
+                document.getElementById('loading').style.display = 'none';
 
+                // Mostrar la imagen procesada
+                const processedImage = document.getElementById('processedImage');
+                processedImage.src = 'data:image/png;base64,' + data.image_with_points_base64;
+                processedImage.style.display = 'block';
+
+                console.log('Imagen original subida con ID:', data.drive_id);
+            } catch (error) {
+                console.error('Error al procesar la imagen:', error);
+                alert("Ocurrió un error al procesar la imagen.");
+                document.getElementById('loading').style.display = 'none';
+            }
+        }
