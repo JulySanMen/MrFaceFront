d95f499e72514fbbdc1754d60b27ea34262c7055
Julieta Sanchez Mendoza <91549989+JulySanMen@users.noreply.github.com>
2024-11-19 12:03:38 -0500

    upluadd

--- resources/js/upload.js
+++ resources/js/upload.js
@@ -1,11 +1,10 @@
-
 // Cambia el texto en el área de subida para mostrar el nombre del archivo
-document.getElementById("file").addEventListener("change", function() {
+document.getElementById("file").addEventListener("change", function () {
     const fileName = this.files[0] ? this.files[0].name : "Haz clic o arrastra una imagen aquí";
     document.querySelector(".upload-icon span").textContent = fileName;
 });
 
 // Dispara el input de tipo archivo al hacer clic en el área completa de subida
-document.getElementById("uploadArea").addEventListener("click", function() {
+document.getElementById("uploadArea").addEventListener("click", function () {
     document.getElementById("file").click();
 });
